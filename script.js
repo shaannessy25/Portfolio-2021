6cfbae13a3968a016b4761118b04300f8b9b06a6
shaannessy25 <shaan.hurley25@gmail.com>
2021-08-16 20:59:37 -0700

    adding project section with project cards, image scroll animation

--- script.js
+++ script.js
@@ -1,86 +1,114 @@
-const mouseCircle = document.querySelector('.mouse-circle');
-const mouseDot = document.querySelector('.mouse-dot');
-
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
 
 const mouseCircleFn = (x, y) => {
-    mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1;`;
-    mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1;`;
-}
+  mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1;`;
+  mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1;`;
+};
 
-
-
-
-// Animated Circles
-const circles = document.querySelectorAll(".circle")
-const mainImg = document.querySelector(".main-circle img")
+/*----------------------Animated Circles---------------------------*/
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
 
 let mX = 0;
 let mY = 0;
 const z = 100;
 const animateCircles = (e, x, y) => {
+  if (x < mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `${z}px`;
+    });
+    mainImg.style.left = `${z}px`;
+  } else if (x > mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `-${z}px`;
+    });
+    mainImg.style.left = `-${z}px`;
+  }
 
-    if(x < mX){
-        circles.forEach(circle=> {
-            circle.style.left = `${z}px`;
-        })
-        mainImg.style.left = `${z}px`;
-    } else if(x > mX){
-        circles.forEach(circle=> {
-            circle.style.left = `-${z}px`;
-        })
-        mainImg.style.left = `-${z}px`;
-    }
+  if (y < mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `${z}px`;
+    });
+    mainImg.style.top = `${z}px`;
+  } else if (y > mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `-${z}px`;
+    });
+    mainImg.style.top = `-${z}px`;
+  }
+  mX = e.clientX;
+  mY = e.clientY;
+};
 
-    if(y < mY){
-        circles.forEach(circle => {
-            circle.style.top = `${z}px`;
-        })
-        mainImg.style.top = `${z}px`;
-    } else if(y > mY){
-        circles.forEach(circle => {
-            circle.style.top = `-${z}px`;
-        })
-        mainImg.style.top = `-${z}px`;
-    }
-    mX = e.clientX
-    mY = e.clientY
+document.body.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
 
-}
+  mouseCircleFn(x, y);
+  animateCircles(e, x, y);
+});
 
+document.body.addEventListener("mouseleave", () => {
+  mouseCircle.style.opacity = "0";
+  mouseDot.style.opacity = "0";
+});
 
-//Main Button
-const mainBtn = document.querySelector(".main-btn");
-let ripple;
+/*----------------------Animated Circles---------------------------*/
 
-mainBtn.addEventListener("mouseenter", (e) => {
+/*----------------------Main Button---------------------------*/
+const mainBtns = document.querySelectorAll(".main-btn");
+mainBtns.forEach((btn) => {
+  let ripple;
+
+  btn.addEventListener("mouseenter", (e) => {
     const left = e.clientX - e.target.getBoundingClientRect().left;
     const top = e.clientY - e.target.getBoundingClientRect().top;
 
-    ripple = document.createElement('div')
-    ripple.classList.add('ripple')
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
     ripple.style.left = `${left}px`;
     ripple.style.top = `${top}px`;
-    mainBtn.prepend(ripple);
+    btn.prepend(ripple);
+  });
 
-})
+  btn.addEventListener("mouseleave", () => {
+    btn.removeChild(ripple);
+  });
+});
 
+/*----------------------Main Button---------------------------*/
 
-mainBtn.addEventListener('mouseleave', () => {
-    mainBtn.removeChild(ripple)
-})
+/*----------------------About Me Text---------------------------*/
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent =
+  "I am a designer & I create awards winning websites with the best user experience & I do not talk much, just contact me. :)";
 
+Array.from(aboutMeTextContent).forEach((char) => {
+  const span = document.createElement("span");
+  span.textContent = char;
+  aboutMeText.appendChild(span);
 
+  span.addEventListener("mouseenter", (e) => {
+    e.target.style.animation = "aboutMeTextAnim 10s infinite";
+  });
+});
 
-document.body.addEventListener('mousemove', (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
+/*----------------------About Me Text---------------------------*/
 
-    mouseCircleFn(x, y);
-    animateCircles(e,x, y);
-})
+/*----------------------Projects---------------------------*/
+const projects = document.querySelectorAll(".project");
 
+projects.forEach((project) => {
+  project.addEventListener("mouseenter", () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
 
-document.body.addEventListener('mouseleave', () => {
-    mouseCircle.style.opacity = '0';
-    mouseDot.style.opacity = '0';
-})
+  project.addEventListener("mouseleave", () => {
+    project.firstElementChild.style.top = "2rem";
+  });
+});
+
+/*----------------------Projects---------------------------*/
